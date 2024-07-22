b5a0e45469034694da509fbfbd6565bd6d49f37b
胡雯淞 <SENSETIME\huwensong@cn0014004123l.domain.sensetime.com>
2024-07-22 17:16:00 +0800

    onnx exported locally

--- pth_to_onnx.py
+++ pth_to_onnx.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("/mnt/afs/huwensong/workspace/R3_factory_tool_arm_calib/runs/detect/train6/weights/best.pt")  # load a custom trained model
+model = YOLO("/home/SENSETIME/huwensong/workspace/r3_factory_tool_arm_calib/best.pt")  # load a custom trained model
 
 # Export the model
 model.export(format="onnx")
